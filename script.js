0115ea020810f8b8b45e0702f5c86ee277f332c3
Frank Lanke Fu Tarimo <fulkast@users.noreply.github.com>
2023-05-17 16:21:05 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+  // Add your JavaScript code here
+
+  // Example: Display a message in the console
+  console.log('Hello, GitHub Pages!');
+});
